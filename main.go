8c8a2e44858dbda32b1a9de1a106141b2dfc130d
pauloestrella <pauloestrella1994@gmail.com>
2022-05-23 22:55:19 -0300

    Data types in go

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference" // := only for variables, syntax sugar
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50 // only positive numbers
+
+	fmt.Printf("conferenceName is %T, remainingTickets is %T, conferenceTickets is %T\n", conferenceName, remainingTickets, conferenceTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
