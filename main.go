7be5079d99f68b36f37c11f835d2ab5fe1cbadad
pauloestrella <pauloestrella1994@gmail.com>
2022-05-25 17:26:15 -0300

    Use scan and pointers to receive values from user and store it

--- main.go
+++ main.go
@@ -13,11 +13,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	// ask for their name
+	// ask for params
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Printf("Enter your first name: ")
+	fmt.Scan(&firstName) // refer a pointer to store value with &
+
+	fmt.Printf("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Printf("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Printf("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
