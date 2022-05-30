683d4b905a4a0626e7a3ff922348331f8355bd51
pauloestrella <pauloestrella1994@gmail.com>
2022-05-30 19:13:37 -0300

    Use if else statement to conditions for tickets avavailability

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets uint = 50 // only positive numbers
 
-	fmt.Printf("conferenceName is %T, remainingTickets is %T, conferenceTickets is %T\n", conferenceName, remainingTickets, conferenceTickets)
-
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
@@ -38,18 +36,28 @@ func main() {
 		fmt.Printf("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - uint(userTickets)
-		bookings = append(bookings, firstName+" "+lastName)
+		if uint(userTickets) <= remainingTickets {
+			remainingTickets = remainingTickets - uint(userTickets)
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		// use _ as a blank identifier to not cause errors of not use variable
-		// in this for loop, _ is the index of the list
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			// use _ as a blank identifier to not cause errors of not use variable
+			// in this for loop, _ is the index of the list
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
 		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			//end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			break
+		}
 	}
 }
